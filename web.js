4c7ee73625293d490fbd85603a10c8c234e121b0
Ubuntu <ubuntu@ip-172-31-20-70.us-west-2.compute.internal>
2013-07-08 00:14:37 +0000

    changed web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ buf = new Buffer(256);
 
 var app = express.createServer(express.logger());
 
-buf = fs.readFileSync('index.html');
+//buf = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
